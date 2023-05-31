fcb40fc47117ac975ec9328fc0b969a645dcaba3
Vithursan1003 <thevendranvithursan@gmail.com>
2023-05-31 10:12:47 +0530

    did some changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { AuthProvider } from "@asgardeo/auth-react";
+import { BASE_URL, CLIENT_ID, CLIENT_SECRET, SIGNIN_REDIRECT_URL, SIGNOUT_REDIRECT_URL } from "./env";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -14,6 +15,15 @@ const config = {
   baseUrl: window.config.base_url,
   scope: ["openid", "profile", "groups", "phone", "email"],
 };
+
+// const config = {
+//   signInRedirectURL: SIGNIN_REDIRECT_URL,
+//   signOutRedirectURL: SIGNOUT_REDIRECT_URL,
+//   clientID: CLIENT_ID,
+//   clientSecret: CLIENT_SECRET,
+//   baseUrl: BASE_URL,
+//   scope: ["openid", "profile", "groups", "phone", "email"],
+// };
 
 root.render(
   <React.StrictMode>

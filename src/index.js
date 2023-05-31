063fc99ff6e77888deb5a8f7980eaa8846699fe4
Vithursan1003 <thevendranvithursan@gmail.com>
2023-05-31 11:50:53 +0530

    did some changes in gramasevaka dashboard

--- src/index.js
+++ src/index.js
@@ -7,26 +7,26 @@ import { BASE_URL, CLIENT_ID, CLIENT_SECRET, SIGNIN_REDIRECT_URL, SIGNOUT_REDIRE
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const config = {
-  signInRedirectURL: window.config.signin_redirect_url,
-  signOutRedirectURL: window.config.signout_redirect_url,
-  clientID: window.config.client_id,
-  clientSecret: window.config.client_secret,
-  baseUrl: window.config.base_url,
-  scope: ["openid", "profile", "groups", "phone", "email"],
-};
+// const config = {
+//   signInRedirectURL: window.config.signin_redirect_url,
+//   signOutRedirectURL: window.config.signout_redirect_url,
+//   clientID: window.config.client_id,
+//   clientSecret: window.config.client_secret,
+//   baseUrl: window.config.base_url,
+//   scope: ["openid", "profile", "groups", "phone", "email"],
+// };
 
 //config credentials
 
 
-// const config = {
-//   signInRedirectURL: SIGNIN_REDIRECT_URL,
-//   signOutRedirectURL: SIGNOUT_REDIRECT_URL,
-//   clientID: CLIENT_ID,
-//   clientSecret: CLIENT_SECRET,
-//   baseUrl: BASE_URL,
-//   scope: ["openid", "profile", "groups", "phone", "email"],
-// };
+const config = {
+  signInRedirectURL: SIGNIN_REDIRECT_URL,
+  signOutRedirectURL: SIGNOUT_REDIRECT_URL,
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  baseUrl: BASE_URL,
+  scope: ["openid", "profile", "groups", "phone", "email"],
+};
 
 root.render(
   <React.StrictMode>
